18f46d3e6fbe1a0aa95ade8cbf8b959183602b05
Shubham-khopade <shubhamrk6555@gmail.com>
2026-06-29 23:10:17 +0530

    Surepass
    1.Change the PAN url and bank verify api url's i the backend.

--- src/main/java/com/project2/ism/request/PanVerificationRequest.java
+++ src/main/java/com/project2/ism/request/PanVerificationRequest.java
@@ -3,8 +3,6 @@ package com.project2.ism.request;
 public class PanVerificationRequest {
 
     private String panNumber;
-    private String fullName;
-    private String dob;
 
     public String getPanNumber() {
         return panNumber;
@@ -14,19 +12,4 @@ public class PanVerificationRequest {
         this.panNumber = panNumber;
     }
 
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getDob() {
-        return dob;
-    }
-
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
 }
